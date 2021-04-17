7d9e6a2f54e2bbe6d92a46d0bb45e6a06dddec89
jai-sharma <sendtojsharma@gmail.com>
2021-04-17 10:51:20 +0530

    making a footerlink component

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,18 @@
 import { Box, HStack, Image, Text, Link } from '@chakra-ui/react';
 
+function FooterLink({ children }) {
+    return (
+        <Link
+            color="#066756"
+            fontSize="18px"
+            fontWeight="700"
+            textDecoration="underline"
+        >
+            {children}
+        </Link>
+    );
+}
+
 export default function Footer() {
     return (
         <Box as="footer" pt="60px" pb="20px" textAlign="center">
@@ -11,33 +24,12 @@ export default function Footer() {
             <Text mt="6">... so you can put your feet up and relax :)</Text>
             <Image src="/assets/relax.png" w="20%" mx="auto" />
             <HStack justifyContent="space-between" w="50%" mx="auto">
-                <Link
-                    color="#066756"
-                    fontSize="18px"
-                    fontWeight="700"
-                    textDecoration="underline"
-                >
-                    Schedule a call
-                </Link>
-                <Link
-                    color="#066756"
-                    fontSize="18px"
-                    fontWeight="700"
-                    textDecoration="underline"
-                >
-                    Join our referral program
-                </Link>
-                <Link
-                    color="#066756"
-                    fontSize="18px"
-                    fontWeight="700"
-                    textDecoration="underline"
-                >
-                    Contact us
-                </Link>
+                <FooterLink>Schedule a call</FooterLink>
+                <FooterLink> Join our referral program</FooterLink>
+                <FooterLink>Contact us</FooterLink>
             </HStack>
             <Box w="80%" mx="auto" my="6" borderTop="1px solid #e4e2e0">
-                <Text color="#69666" fontSize="12px" pt="4" >
+                <Text color="#69666" fontSize="12px" pt="4">
                     @Copyright 2021 · Designwiz
                 </Text>
             </Box>
