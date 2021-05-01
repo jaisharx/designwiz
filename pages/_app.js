18f90f65827e4cb9f786cfe2b6c7d120e90c5fa1
jai-sharma <sendtojsharma@gmail.com>
2021-05-01 10:22:55 +0530

    adding seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import NextHead from 'components/next-head';
+import content from 'lib/seo';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <>
+            <NextHead
+                url={content.url}
+                favicon={content.favicon}
+                title={content.title}
+                desc={content.desc}
+                seoImage={content.seoImage}
+            />
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-
 import Navbar from 'components/navbar';
 import Header from 'components/header';
 import FeatureSection from 'components/sections/features';
@@ -12,11 +10,6 @@ import Footer from 'components/footer';
 export default function Home() {
     return (
         <>
-            <Head>
-                <title>designwiz</title>
-                <link rel="icon" href="/favicon.png" />
-            </Head>
-
             <Navbar />
             <Header />
             <FeatureSection />
