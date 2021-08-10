d366ccc8d0153c06d7bcfec73fb1dbbbb2f59ef2
jai-sharma <sendtojsharma@gmail.com>
2021-08-10 08:31:02 +0530

    adding priority to images above the fold

--- components/header.tsx
+++ components/header.tsx
@@ -1,85 +1,85 @@
-import { Badge, Box, Flex, Heading, HStack, Text } from '@chakra-ui/react';
-import Container from 'components/container';
-import NextImage from 'next/image';
-import CTAButton from './button';
+import { Badge, Box, Flex, Heading, HStack, Text } from '@chakra-ui/react'
+import Container from 'components/container'
+import NextImage from 'next/image'
+import CTAButton from './button'
 
 function SocialBrandIcon({ src }) {
-    return (
-        <Box w="36">
-            <NextImage
-                width="300"
-                height="100"
-                alt="Trusted Brands"
-                src={`/assets/social/${src}.png`}
-            />
-        </Box>
-    );
+  return (
+    <Box w="36">
+      <NextImage
+        width="300"
+        height="100"
+        alt="Trusted Brands"
+        src={`/assets/social/${src}.png`}
+      />
+    </Box>
+  )
 }
 
 function SocialProof() {
-    return (
-        <Box mt="44">
-            <Text textAlign="center" color="#d6c0a7">
-                Trusted by these amazing brands
-            </Text>
-            <HStack spacing="16" mt="3">
-                <SocialBrandIcon src="1" />
-                <SocialBrandIcon src="2" />
-                <SocialBrandIcon src="3" />
-                <SocialBrandIcon src="4" />
-                <SocialBrandIcon src="5" />
-            </HStack>
-        </Box>
-    );
+  return (
+    <Box mt="44">
+      <Text textAlign="center" color="#d6c0a7">
+        Trusted by these amazing brands
+      </Text>
+      <HStack spacing="16" mt="3">
+        <SocialBrandIcon src="1" />
+        <SocialBrandIcon src="2" />
+        <SocialBrandIcon src="3" />
+        <SocialBrandIcon src="4" />
+        <SocialBrandIcon src="5" />
+      </HStack>
+    </Box>
+  )
 }
 
 export default function Header() {
-    return (
-        <Box
-            as="header"
-            bgGradient="linear-gradient(180deg, #fff, #fce8d1)"
-            py="12"
-        >
-            <Container>
-                <Flex alignItems="center">
-                    <Box>
-                        <HStack>
-                            <Badge
-                                backgroundColor="yellow.700"
-                                variant="solid"
-                                p="6px 10px"
-                                borderRadius="4"
-                            >
-                                NEW!
-                            </Badge>
-                            <Text color="yellow.600">
-                                Unlimited front-end development with Webflow
-                            </Text>
-                        </HStack>
-                        <Heading as="h1" py="4" fontSize="42px">
-                            Delightful high-quality design. Reliable and
-                            affordable.
-                        </Heading>
-                        <Text fontSize="20px">
-                            Unlimited design requests to grow your business
-                        </Text>
-                        <CTAButton mt="6" colorScheme="green" size="lg">
-                            Get Started
-                        </CTAButton>
-                    </Box>
+  return (
+    <Box
+      as="header"
+      bgGradient="linear-gradient(180deg, #fff, #fce8d1)"
+      py="12"
+    >
+      <Container>
+        <Flex alignItems="center">
+          <Box>
+            <HStack>
+              <Badge
+                backgroundColor="yellow.700"
+                variant="solid"
+                p="6px 10px"
+                borderRadius="4"
+              >
+                NEW!
+              </Badge>
+              <Text color="yellow.600">
+                Unlimited front-end development with Webflow
+              </Text>
+            </HStack>
+            <Heading as="h1" py="4" fontSize="42px">
+              Delightful high-quality design. Reliable and affordable.
+            </Heading>
+            <Text fontSize="20px">
+              Unlimited design requests to grow your business
+            </Text>
+            <CTAButton mt="6" colorScheme="green" size="lg">
+              Get Started
+            </CTAButton>
+          </Box>
 
-                    <Box w="98%">
-                        <NextImage
-                            width="1076"
-                            height="1080"
-                            alt="Hero Image"
-                            src="/assets/rocket.png"
-                        />
-                    </Box>
-                </Flex>
+          <Box w="98%">
+            <NextImage
+              priority
+              width="1076"
+              height="1080"
+              alt="Hero Image"
+              src="/assets/rocket.png"
+            />
+          </Box>
+        </Flex>
 
-                <SocialProof />
-            </Container>
-        </Box>
-    );
+        <SocialProof />
+      </Container>
+    </Box>
+  )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,30 @@
-import { Box, Flex, Button } from '@chakra-ui/react';
-import Container from 'components/container';
-import NextImage from 'next/image';
-import CTAButton from './button';
+import { Box, Flex, Button } from '@chakra-ui/react'
+import Container from 'components/container'
+import NextImage from 'next/image'
+import CTAButton from './button'
 
-const btnHoverState = { backgroundColor: 'none', color: 'green.600' };
+const btnHoverState = { backgroundColor: 'none', color: 'green.600' }
 export default function Navbar() {
-    return (
-        <Box as="nav" py="6">
-            <Container>
-                <Flex alignItems="center">
-                    <Flex mr="auto" w="22%">
-                        <NextImage
-                            alt="Logo"
-                            src="/assets/logo.png"
-                            width="400"
-                            height="100"
-                            objectFit="cover"
-                        />
-                    </Flex>
-                    <Button variant="ghost" _hover={btnHoverState}>
-                        Login
-                    </Button>
-                    <CTAButton>Get Started</CTAButton>
-                </Flex>
-            </Container>
-        </Box>
-    );
+  return (
+    <Box as="nav" py="6">
+      <Container>
+        <Flex alignItems="center">
+          <Flex mr="auto" w="22%">
+            <NextImage
+              priority
+              alt="Logo"
+              src="/assets/logo.png"
+              width="400"
+              height="100"
+              objectFit="cover"
+            />
+          </Flex>
+          <Button variant="ghost" _hover={btnHoverState}>
+            Login
+          </Button>
+          <CTAButton>Get Started</CTAButton>
+        </Flex>
+      </Container>
+    </Box>
+  )
 }
